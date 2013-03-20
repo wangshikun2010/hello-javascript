b488a88bb6b01656712a684c58282fd5aa7dfecf
wangshikun2010@gmail.com <wangshikun2010@gmail.com@b33ea24b-0e5b-8e1c-e15a-08057897790a>
2013-03-20 13:09:01 +0000

    计算器程序的修改
    git-svn-id: https://my-javascript-jquery.googlecode.com/svn/trunk@6 b33ea24b-0e5b-8e1c-e15a-08057897790a

--- javascript入门经典/Calculator/Calculator.js
+++ javascript入门经典/Calculator/Calculator.js
@@ -1,38 +1,55 @@
 var oper_states; //操作状态
 var operator; //操作符
 var num1; //第一个数
+var scores = [];
 
 //设置开始状态
-function setStartState() {
+window.onload = function setStartState() {
 	oper_states = false;
 	operator = "isempty";
 	num1 = 0;
+	var calc_text = document.cal.calc_text;
+	li_event();
 }
 
 //添加数字
 function add(number) {
 	if (oper_states) {
-		document.cal.calc_text.value = number;
+		calc_text.value = number;
 		oper_states = false;
 	} else {
-		if (document.cal.calc_text.value == "0") {
-			document.cal.calc_text.value = number;
+		if (calc_text.value == "0") {
+			calc_text.value = number;
 		} else {
-			document.cal.calc_text.value += number;
+			calc_text.value += number;
+		}
+	}
+}
+
+scores = document.getElementsByTagName('li');
+function li_event() {
+	for (var i=0; i<scores.length; i++) {
+		switch(scores[i].innerHTML) {
+			case '1':
+			scores[i].addEventListener('click',add('1'),false);
+			case '2':
+			scores[i].addEventListener('click',add('2'),false);
+			case '3':
+			scores[i].addEventListener('click',add('3'),false);
 		}
 	}
 }
 
 //添加小数点
 function addPoint() {
-	document.cal.calc_text.value += ".";
+	calc_text.value += ".";
 }
 
 //操作符
 function setOper(oper) {
 	oper_states = true;
 	if (operator == "isempty") {
-		num1 = 0 + document.cal.calc_text.value;
+		num1 = 0 + calc_text.value;
 		operator = oper;
 	} else {
 		oper = oper.charAt(oper.length - 1);
@@ -46,25 +63,25 @@ function count() {
 	if (operator != "isempty") {
 		switch(operator) {
 			case "+":
-			num1 = parseFloat(num1) + parseFloat(document.cal.calc_text.value);
+			num1 = parseFloat(num1) + parseFloat(calc_text.value);
 			break;
 			case "-":
-			num1 = parseFloat(num1) - parseFloat(document.cal.calc_text.value);
+			num1 = parseFloat(num1) - parseFloat(calc_text.value);
 			break;
 			case "*":
-			num1 = parseFloat(num1) * parseFloat(document.cal.calc_text.value);
+			num1 = parseFloat(num1) * parseFloat(calc_text.value);
 			break;
 			case "/":
-			num1 = parseFloat(num1) / parseFloat(document.cal.calc_text.value);
+			num1 = parseFloat(num1) / parseFloat(calc_text.value);
 			break;
 		}
-		document.cal.calc_text.value = num1;
+		calc_text.value = num1;
 	}
 	operator = "isempty";
 }
 
 //清空数据
 function clearText() {
-	document.cal.calc_text.value = "0";
+	calc_text.value = "0";
 	operator = "isempty";
 }
