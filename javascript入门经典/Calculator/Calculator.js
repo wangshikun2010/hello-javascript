97c52e8e9e9bb27e30a91f333332d5ae0aa33a63
wangshikun2010@gmail.com <wangshikun2010@gmail.com@b33ea24b-0e5b-8e1c-e15a-08057897790a>
2013-03-20 05:12:40 +0000

    计算器程序实现最基本的计算功能
    git-svn-id: https://my-javascript-jquery.googlecode.com/svn/trunk@5 b33ea24b-0e5b-8e1c-e15a-08057897790a

--- javascript入门经典/Calculator/Calculator.js
+++ javascript入门经典/Calculator/Calculator.js
@@ -1,23 +1,70 @@
-var operstates; //操作符
-var number1; //第一个数字
+var oper_states; //操作状态
+var operator; //操作符
+var num1; //第一个数
 
-//初始状态
+//设置开始状态
 function setStartState() {
-	operstate = false;
-	number1 = 0;
-}
-
-//清空数据
-function calc_cleartext() {
-	document.cal.calc_text.value = '0';
+	oper_states = false;
+	operator = "isempty";
+	num1 = 0;
 }
 
 //添加数字
-function add(num) {
-	if (document.cal.calc_text.value == '0') {
-		document.cal.calc_text.value = num;
+function add(number) {
+	if (oper_states) {
+		document.cal.calc_text.value = number;
+		oper_states = false;
 	} else {
-		document.cal.calc_text.value += num;
+		if (document.cal.calc_text.value == "0") {
+			document.cal.calc_text.value = number;
+		} else {
+			document.cal.calc_text.value += number;
+		}
 	}
 }
 
+//添加小数点
+function addPoint() {
+	document.cal.calc_text.value += ".";
+}
+
+//操作符
+function setOper(oper) {
+	oper_states = true;
+	if (operator == "isempty") {
+		num1 = 0 + document.cal.calc_text.value;
+		operator = oper;
+	} else {
+		oper = oper.charAt(oper.length - 1);
+		operator = oper;
+		count();
+	}
+}
+
+//计算结果
+function count() {
+	if (operator != "isempty") {
+		switch(operator) {
+			case "+":
+			num1 = parseFloat(num1) + parseFloat(document.cal.calc_text.value);
+			break;
+			case "-":
+			num1 = parseFloat(num1) - parseFloat(document.cal.calc_text.value);
+			break;
+			case "*":
+			num1 = parseFloat(num1) * parseFloat(document.cal.calc_text.value);
+			break;
+			case "/":
+			num1 = parseFloat(num1) / parseFloat(document.cal.calc_text.value);
+			break;
+		}
+		document.cal.calc_text.value = num1;
+	}
+	operator = "isempty";
+}
+
+//清空数据
+function clearText() {
+	document.cal.calc_text.value = "0";
+	operator = "isempty";
+}
